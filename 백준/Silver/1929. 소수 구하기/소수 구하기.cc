7d4ff614dcaa55154cfe29601bc300d9ec601abb
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-11-17 23:38:30 +0900

    [Silver III] Title: 소수 구하기, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -5,9 +5,11 @@ using namespace std;
 bool isPrime(int n)
 {
     if (n == 1) return false;
-    for (int i = 2; i * i <= n; i++)
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i = i + 6)
     {
-        if (n % i == 0) return false;
+        if (n % i == 0 || n % (i + 2) == 0) return false;
     }
     return true;
 }
